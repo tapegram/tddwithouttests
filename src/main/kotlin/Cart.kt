5c91cc5421eb863ca06fb5f9cbd049405d877cb5
Tavish Pegram <tapegram@gmail.com>
2020-06-15 22:19:01 -0500

    can add items to the cart

--- src/main/kotlin/Cart.kt
+++ src/main/kotlin/Cart.kt
@@ -1,3 +1,12 @@
-data class Cart(
+class Cart(
+    val id: String,
+    val items: MutableList<Item> = mutableListOf()
+) {
+    fun addItem(item: Item) {
+        items.add(item)
+    }
+}
+
+data class Item(
     val id: String
 )
--- src/test/kotlin/CartTest.kt
+++ src/test/kotlin/CartTest.kt
@@ -8,4 +8,40 @@ class CartTest {
             cart.id == "123"
         )
     }
+
+    @Test
+    fun `can add an item to a cart`() {
+        val cart = Cart(id="123")
+        cart.addItem(
+            Item(
+                id = "item1"
+            )
+        )
+
+        assert(
+            cart.items == listOf(Item(id = "item1"))
+        )
+    }
+
+    @Test
+    fun `can add multiple items to the cart`() {
+        val cart = Cart(id="123")
+        cart.addItem(
+            Item(
+                id = "item1"
+            )
+        )
+        cart.addItem(
+            Item(
+                id = "item2"
+            )
+        )
+
+        assert(
+            cart.items == listOf(
+                Item(id = "item1"),
+                Item(id = "item2")
+            )
+        )
+    }
 }
